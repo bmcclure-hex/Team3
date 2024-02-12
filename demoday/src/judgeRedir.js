fad3322422b1461f39b2452cdd96c3d26b92aa46
Nehemie Polifond <npolifond@gmail.com>
2024-02-12 00:04:07 -0500

    Getting Started on Judge view

---
+++ demoday/src/judgeRedir.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+        <AppBar>
+            
+        </AppBar>
+      <header>
+        <h1>Judge View</h1>
+        
+      </header>
+    </div>
+  )
+}
