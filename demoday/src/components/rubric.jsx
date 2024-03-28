6803e3ad16cdc04e5f5adf53935d21ecd839af43
Nehemie Polifond <npolifond@gmail.com>
2024-03-28 18:30:54 -0400

    Score submission validations
    All question must now be answered before being submitted

--- demoday/src/components/rubric.jsx
+++ demoday/src/components/rubric.jsx
@@ -1,130 +1,251 @@
-import PropTypes from 'prop-types'
-import { Grid,Button, FormControl, Input, TextField, Paper, Typography,FormControlLabel, RadioGroup } from '@mui/material'
-import { useState } from 'react'
-import React from 'react'
-import Ratings from '../components/radioButtonRatings'
+import { Grid, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import React from "react";
+import Ratings from "../components/radioButtonRatings";
 
 const Rubric = () => {
-    const[formData,setFormData] = useState({
-        //needs to updated for 
-        presentation: '',
-        content: '',
-        content2:'',
-        ox: '',
-        ux: '',
-        ux2:'',
-        tech:'',
-        tech2:'',
-        comments: ''
-    })
+  const [error, setError] = React.useState(false);
+  const [formData, setFormData] = useState({
+    presentation: "",
+    content: "",
+    content2: "",
+    ox: "",
+    ux: "",
+    ux2: "",
+    tech: "",
+    tech2: "",
+    comments: "",
+  });
 
-    //allows selection
-   const handleChange = (e)=>{
-    const {name ,value} = e.target;
-    setFormData({...formData,[name]:value});
-   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    console.log(e.target.value);
+  };
 
-   return (
+  const submitRubric = (e) => {
+    e.preventDefault();
+
+    const isAnyFieldEmpty = Object.values(formData).some(
+      (value) => value === ""
+    );
+
+    if (isAnyFieldEmpty) {
+      setError(true);
+      alert("Error: Please complete all questions before submitting.");
+    } else {
+      // All fields are filled, proceed with form submission
+      setError(false);
+      // submission completed
+      alert("submitted");
+    }
+  };
+
+  return (
     <div>
-        <form>
-            <Grid container spacing={4}>
+      <form onSubmit={submitRubric}>
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <Typography
+              style={{
+                marginBottom: "16px",
+                color: formData.presentation ? "inherit" : "red",
+              }}
+              sx={{ textDecoration: "underline" }}
+              variant="h5"
+            >
+              Presentation{" "}
+            </Typography>
 
-                <Grid item xs={12}>
-                    <Typography style={{ marginBottom: '16px' }}  sx={{textDecoration: 'underline'}} variant='h5'>Presentation </Typography>
+            <Typography variant="h6">
+              How clear and concise was the presentation of the project?
+            </Typography>
+            <Ratings
+              name="presentation"
+              value={formData.presentation}
+              onChange={handleChange}
+              options={[
+                "Very Clear",
+                "Clear",
+                "Neutral",
+                "Unclear",
+                "Very Unclear",
+              ]}
+            />
+          </Grid>
 
-                    <Typography variant='h6'>How clear and concise was the presentation of the project?</Typography>
-                    <Ratings
-                        name="presentation"
-                        value={formData.presentation}
-                        onChange={handleChange}
-                        options={["Very Clear", "Clear", "Neutral", "Unclear", "Very Unclear"]}
-                    />
-                </Grid>
+          <Grid item xs={12}>
+            <Typography
+              style={{ marginBottom: "16px" }}
+              sx={{ textDecoration: "underline" }}
+              variant="h5"
+            >
+              Content{" "}
+            </Typography>
 
-                <Grid item xs={12}>
-                    <Typography style={{ marginBottom: '16px' }} sx={{textDecoration: 'underline'}} variant='h5'>Content </Typography>
+            <Typography variant="h6">
+              Did the project address a relevant problem or need in the field of
+              Computer Science?
+            </Typography>
+            <Ratings
+              name="content"
+              value={formData.content}
+              onChange={handleChange}
+              options={[
+                "Very Clear",
+                "Clear",
+                "Neutral",
+                "Unclear",
+                "Very Unclear",
+              ]}
+            />
+            <Typography variant="h6">
+              How innovative did you find the project concept or solution?
+            </Typography>
+            <Ratings
+              name="content2"
+              value={formData.content2}
+              onChange={handleChange}
+              options={[
+                "Very Clear",
+                "Clear",
+                "Neutral",
+                "Unclear",
+                "Very Unclear",
+              ]}
+            />
+          </Grid>
 
-                    <Typography variant='h6'>Did the project address a relevant problem or need in the field of Computer Science?</Typography>
-                    <Ratings
-                        name="content"
-                        value={formData.content}
-                        onChange={handleChange}
-                        options={["Very Clear", "Clear", "Neutral", "Unclear", "Very Unclear"]}
-                    />
-                    <Typography variant='h6'>How innovative did you find the project concept or solution?</Typography>
-                    <Ratings
-                        name="content2"
-                        value={formData.content2}
-                        onChange={handleChange}
-                        options={["Very Clear", "Clear", "Neutral", "Unclear", "Very Unclear"]}
-                        
-                    />
-                </Grid>
+          <Grid item xs={12}>
+            <Typography
+              style={{ marginBottom: "16px" }}
+              sx={{ textDecoration: "underline" }}
+              variant="h5"
+            >
+              Technical Execution
+            </Typography>
 
-                <Grid item xs={12}>
-                    <Typography style={{ marginBottom: '16px' }}  sx={{textDecoration: 'underline'}} variant='h5'>Technical Execution</Typography>
+            <Typography variant="h6">
+              How well was the project technically executed?
+            </Typography>
+            <Ratings
+              name="tech"
+              value={formData.tech}
+              onChange={handleChange}
+              options={[
+                "Very Clear",
+                "Clear",
+                "Neutral",
+                "Unclear",
+                "Very Unclear",
+              ]}
+            />
+            <Typography variant="h6">
+              Were you impressed by the technical skills demonstrated in the
+              project?
+            </Typography>
+            <Ratings
+              name="tech2"
+              value={formData.tech2}
+              onChange={handleChange}
+              options={[
+                "Very Clear",
+                "Clear",
+                "Neutral",
+                "Unclear",
+                "Very Unclear",
+              ]}
+            />
+          </Grid>
 
-                    <Typography variant='h6'>How well was the project technically executed?</Typography>
-                    <Ratings
-                        name="tech"
-                        value={formData.tech}
-                        onChange={handleChange}
-                        options={["Very Clear", "Clear", "Neutral", "Unclear", "Very Unclear"]}
-                    />
-                    <Typography variant='h6'>Were you impressed by the technical skills demonstrated in the project?</Typography>
-                    <Ratings
-                        name="tech2"
-                        value={formData.tech2}
-                        onChange={handleChange}
-                        options={["Very Clear", "Clear", "Neutral", "Unclear", "Very Unclear"]}
-                    />
-                </Grid>
+          <Grid item xs={12}>
+            <Typography
+              style={{ marginBottom: "16px" }}
+              sx={{ textDecoration: "underline" }}
+              variant="h5"
+            >
+              User Experience
+            </Typography>
+            <Typography variant="h6">
+              How user-friendly was the project?
+            </Typography>
+            <Ratings
+              name="ux"
+              value={formData.ux}
+              onChange={handleChange}
+              options={[
+                "Very Clear",
+                "Clear",
+                "Neutral",
+                "Unclear",
+                "Very Unclear",
+              ]}
+            />
+            <Typography variant="h6">
+              Did the project provide a positive user experience?
+            </Typography>
+            <Ratings
+              name="ux2"
+              value={formData.ux2}
+              onChange={handleChange}
+              options={[
+                "Very Clear",
+                "Clear",
+                "Neutral",
+                "Unclear",
+                "Very Unclear",
+              ]}
+            />
+          </Grid>
 
-                <Grid item xs={12}>
-                    <Typography style={{ marginBottom: '16px' }}  sx={{textDecoration: 'underline'}} variant='h5'>User Experience</Typography>
-                    <Typography variant='h6'>How user-friendly was the project?</Typography>
-                    <Ratings
-                        name="ux"
-                        value={formData.ux}
-                        onChange={handleChange}
-                        options={["Very Clear", "Clear", "Neutral", "Unclear", "Very Unclear"]}
-                    />
-                    <Typography variant='h6'>Did the project provide a positive user experience?</Typography>
-                    <Ratings
-                        name="ux2"
-                        value={formData.ux2}
-                        onChange={handleChange}
-                        options={["Very Clear", "Clear", "Neutral", "Unclear", "Very Unclear"]}
-                    />
-                </Grid>
+          <Grid item xs={12}>
+            <Typography
+              style={{ marginBottom: "16px" }}
+              sx={{ textDecoration: "underline" }}
+              variant="h5"
+            >
+              Overall Experience
+            </Typography>
+            <Typography variant="h6">
+              How user-friendly was the project?
+            </Typography>
+            <Ratings
+              name="ox"
+              value={formData.ox}
+              onChange={handleChange}
+              options={["Excellent", "Very Good", "Good", "Fair", "Poor"]}
+            />
+          </Grid>
 
-                <Grid item xs={12}>
-                    <Typography style={{ marginBottom: '16px' }}  sx={{textDecoration: 'underline'}} variant='h5'>Overall Experience</Typography>
-                    <Typography variant='h6'>How user-friendly was the project?</Typography>
-                    <Ratings
-                        name="ox"
-                        value={formData.ox}
-                        onChange={handleChange}
-                        options={["Excellent", "Very Good", "Good", "Fair", "Poor"]}
-                    />
-                </Grid>
+          <Grid item xs={12}>
+            <Typography
+              style={{ marginBottom: "16px" }}
+              sx={{ textDecoration: "underline" }}
+              variant="h5"
+            >
+              Additional Comments
+            </Typography>
+            <Typography variant="h6">
+              Please provide any suggestions or feedback for improving the
+              project or presentation:
+            </Typography>
+            <TextField
+              name="comments"
+              value={formData.comments}
+              onChange={handleChange}
+            ></TextField>
+          </Grid>
 
-                <Grid item xs={12}>
-                    <Typography style={{ marginBottom: '16px' }} sx={{textDecoration: 'underline'}} variant='h5'>Additional Comments</Typography>
-                    <Typography variant='h6' name="comments" value={formData.comments}>Please provide any suggestions or feedback for improving the project or presentation:</Typography>
-                    <TextField></TextField>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Button variant="contained"> Submit</Button>
-                </Grid>
-
-            </Grid>
-
-        </form>
-
+          <Grid item xs={12}>
+            <Button variant="contained" type="submit">
+              {" "}
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
     </div>
-)
-}
+  );
+};
 
-export default Rubric
+export default Rubric;
--- demoday/src/pages/scorePage.jsx
+++ demoday/src/pages/scorePage.jsx
@@ -1,39 +1,31 @@
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
-import React from 'react';
-import Rubric from '../components/rubric'
-import Project from '../components/projectCard'
-import { Button, Typography } from '@mui/material';
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import React from "react";
+import Rubric from "../components/rubric";
+import Project from "../components/projectCard";
+import { Typography } from "@mui/material";
 
-  
-
-function scorePage(){
-   return(
-    <div style={{
-        margin: '20px 0px 0px 20px'
-    }}>
-    <Typography align='center' sx={{my:5}} variant='h4'>Project Scoring Rubric</Typography>
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <Paper elevation={3} sx={{ p: 5 }}>
-          <Rubric />
-        </Paper>
-      </Grid>
-      <Grid item xs={12} md={6}>
+function scorePage() {
+  return (
+    <div
+      style={{
+        margin: "20px 0px 0px 20px",
+      }}
+    >
+      <Typography align="center" sx={{ my: 5 }} variant="h4">
+        Project Scoring Rubric
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} sx={{ p: 5 }}>
+            <Rubric />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
           <Project />
+        </Grid>
       </Grid>
-    </Grid>
-    <Button>Submit</Button>
-  </div>
-
- 
-    
-   );
-
-   
+    </div>
+  );
 }
 export default scorePage;
-
